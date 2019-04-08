01dfd18cacd4de98ad9a0ea6c46d3ca07ea07dfc
Totally Not Going To Tell <neoh.wilson@gmail.com>
2019-04-08 18:27:18 +0800

    Added special power indicator

--- jpyf_actual/Assets/Scripts/Wilson's Script/Daylight.cs
+++ jpyf_actual/Assets/Scripts/Wilson's Script/Daylight.cs
@@ -5,16 +5,39 @@ using UnityEngine;
 public class Daylight : MonoBehaviour
 {
     float dayTimeTimer;
-    bool isDayTime;
+    float nightTimeTimer;
+    bool isChangingDayNight;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isChangingDayNight = false;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        dayTimeTimer += Time.deltaTime;
+        transform.RotateAround(Vector3.zero, Vector3.right, 0);
+
+        Debug.Log(dayTimeTimer);
+        if (dayTimeTimer > 3f)
+        {
+            isChangingDayNight = true;
+            dayTimeTimer = 0f;
+        }
+
+        if (isChangingDayNight == true)
+            RotatingSun();
+
+        if(isChangingDayNight == true && dayTimeTimer > 3)
+        {
+            isChangingDayNight = true;
+            dayTimeTimer = 0f;
+        }
+    }
+
+    void RotatingSun()
     {
         transform.RotateAround(Vector3.zero, Vector3.right, 20f * Time.deltaTime);
         transform.LookAt(Vector3.zero);
---
+++ jpyf_actual/Assets/Scripts/Wilson's Script/SPindicator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPindicator : MonoBehaviour
+{
+    public GameObject mainObject;
+    public float speed = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OrbitAround();
+    }
+
+    void OrbitAround()
+    {
+        transform.RotateAround(mainObject.transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}

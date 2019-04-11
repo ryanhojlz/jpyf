717063cc338d554ee9232a3a05fb75c46dfc16f8
Totally Not Going To Tell <neoh.wilson@gmail.com>
2019-04-11 11:30:11 +0800

    added playtime

--- jpyf_actual/Assets/Scripts/ZiJun's Script/Achievement & Statistics/StatisticDisplay.cs
+++ jpyf_actual/Assets/Scripts/ZiJun's Script/Achievement & Statistics/StatisticDisplay.cs
@@ -7,18 +7,18 @@ public class StatisticDisplay : MonoBehaviour
 {
     public Text numberWin;
     public Text numberLose;
-    //public Text totalPlayTime;
-    
+    public Text totalPlayTime;
+    public int Hour;
+    public int Min;
+
     // Use this for initialization
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         numberWin.text = "" + GameObject.Find("StatsManager").GetComponent<Statistics>().Data.number_win;
         numberLose.text = "" + GameObject.Find("StatsManager").GetComponent<Statistics>().Data.number_lose;
+
+        totalPlayTime.text = "" + GameObject.Find("StatsManager").GetComponent<Statistics>().Data.Hour + " hr : " + GameObject.Find("StatsManager").GetComponent<Statistics>().Data.Min + " min";
     }
 }
--- jpyf_actual/Assets/Scripts/ZiJun's Script/Achievement & Statistics/Statistics.cs
+++ jpyf_actual/Assets/Scripts/ZiJun's Script/Achievement & Statistics/Statistics.cs
@@ -18,11 +18,16 @@ public class Statistics : MonoBehaviour
 
     //public Text numberWin;
     //public Text numberLose;
-
+    //public Text dateTime;
     public struct StatisticsData
     {
         public int number_win;
         public int number_lose;
+        public float playTime;
+
+        public int Hour;
+        public int Min;
+        public float Sec;
     }
 
     public StatisticsData Data = new StatisticsData();
@@ -32,21 +37,25 @@ public class Statistics : MonoBehaviour
     {
         //loading value
         DontDestroyOnLoad(transform.gameObject);
+        Data.playTime = 0f;
 
-        //StatisticsData statisticsData = new StatisticsData();
-        //Data.number_win = 50;
-        //Data.number_lose = 10;
-        ////string json = JsonUtility.ToJson(statisticsData);
+        Data.Hour = 0;
+        Data.Min = 0;
+        Data.Sec = 0f;
+    //StatisticsData statisticsData = new StatisticsData();
+    //Data.number_win = 50;
+    //Data.number_lose = 10;
+    ////string json = JsonUtility.ToJson(statisticsData);
 
-        //string json = JsonUtility.ToJson(Data);
-        //File.WriteAllText(Application.dataPath + "saveFile.json", json);
-        //string json = File.ReadAllText(Application.dataPath + "/saveFile.json");
+    //string json = JsonUtility.ToJson(Data);
+    //File.WriteAllText(Application.dataPath + "saveFile.json", json);
+    //string json = File.ReadAllText(Application.dataPath + "/saveFile.json");
 
-        //StatisticsData loadedStatisticsData = JsonUtility.FromJson<StatisticsData>(json);
-        //Debug.Log(loadedStatisticsData.number_win);
-        //Debug.Log(loadedStatisticsData.number_lose);
+    //StatisticsData loadedStatisticsData = JsonUtility.FromJson<StatisticsData>(json);
+    //Debug.Log(loadedStatisticsData.number_win);
+    //Debug.Log(loadedStatisticsData.number_lose);
 
-        LoadStats();
+    LoadStats();
 
         Debug.Log(Data.number_win);
     }
@@ -57,6 +66,20 @@ public class Statistics : MonoBehaviour
         //updating value
         //numberWin.text = "" + Data.number_win;
         //numberLose.text = "" + Data.number_lose;
+        //dateTime.text = System.DateTime.Now.ToString();
+        //Debug.Log(dateTime.text);
+        Data.Sec += Time.deltaTime;
+
+        if (Data.Sec > 59)
+        {
+            Data.Min += 1;
+            Data.Sec = 0;
+        }
+        if (Data.Min > 59)
+        {
+            Data.Hour += 1;
+            Data.Min = 0;
+        }
     }
 
     public void SaveStats()
@@ -87,13 +110,11 @@ public class Statistics : MonoBehaviour
     public void incrementWin()
     {
         Data.number_win++;
-        SaveStats();
     }
 
     public void incrementLose()
     {
         Data.number_lose++;
-        SaveStats();
     }
 
     public void SetWin(int value)
@@ -106,5 +127,16 @@ public class Statistics : MonoBehaviour
         Data.number_lose = value;
     }
 
+    public void ResetSave()
+    {
+        Data.number_win = 0;
+        Data.number_lose = 0;
+        SaveStats();
+    }
 
+    private void OnDestroy()
+    {
+        SaveStats();
+        ResetSave();
+    }
 }

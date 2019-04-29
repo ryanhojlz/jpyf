f99e40ab747648fb0c69fab189275dabb4d9959e
Totally Not Going To Tell <neoh.wilson@gmail.com>
2019-04-29 18:22:07 +0800

    Added the fading thing

--- jpyf_actual/Assets/#Prototype_new/Wilson/Script/DayNightCycle.cs
+++ jpyf_actual/Assets/#Prototype_new/Wilson/Script/DayNightCycle.cs
@@ -42,13 +42,13 @@ public class DayNightCycle : MonoBehaviour
         if ((sunRotation < 90 && sunRotation > 0) || (sunRotation > 270 && sunRotation < 360))
         {
             isDaytime = true;
-            Debug.Log("Daytime " + isDaytime);
+            //Debug.Log("Daytime " + isDaytime);
         }
 
         if (sunRotation > 90 && sunRotation < 270)
         {
             isDaytime = false;
-            Debug.Log("Night " + isDaytime);
+            //Debug.Log("Night " + isDaytime);
         }
     }
 

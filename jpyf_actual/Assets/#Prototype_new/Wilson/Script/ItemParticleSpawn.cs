c514cb2585f5511c2df0db23c9e341b41df5d5ef
Totally Not Going To Tell <neoh.wilson@gmail.com>
2019-05-02 18:48:33 +0800

    Item effect done

---
+++ jpyf_actual/Assets/#Prototype_new/Wilson/Script/ItemParticleSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemParticleSpawn : MonoBehaviour
+{
+    float timer = 0f;
+    //private ParticleSystem ps;
+    // Use this for initialization
+    void Start()
+    {
+        //ps = GetComponent<ParticleSystem>();
+        //timer = ps.duration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 2f) 
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}

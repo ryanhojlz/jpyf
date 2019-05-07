821ffab69e1354eed4e48c8762c8930b2dc21d92
zijuntok <48231216+zijuntok@users.noreply.github.com>
2019-05-07 10:46:51 +0800

    Added hord spawner && hord unit ramming
    ZiJun -> Hord_Nurakabe -> Hord_Nurakabe_Spawner

---
+++ jpyf_actual/Assets/#Prototype_new/ZiJun/Hord_Nurakabe/Entity_Hord_Nurakabe.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity_Hord_Nurakabe : MonoBehaviour
+{
+    public int m_damage;
+    public float m_moveSpeed;
+    public Vector3 dir = Vector3.zero;
+
+    Stats_ResourceScript handler = null;
+    // Use this for initialization
+    void Start()
+    {
+        handler = Stats_ResourceScript.Instance;
+        dir.Set(0, 0, -1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += this.transform.forward * m_moveSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Payload")
+        {
+            AttackCart();
+        }
+        else if (other.tag == "Player2")
+        {
+            AttackPlayer();
+        }
+    }
+
+    void AttackPlayer()
+    {
+        //Debug.Log("Player_Got_Hit");
+        handler.Player2_TakeDmg(m_damage);
+    }
+
+    void AttackCart()
+    {
+        //Debug.Log("Cart_Got_Hit");
+        handler.Cart_TakeDmg(m_damage);
+    }
+}
---
+++ jpyf_actual/Assets/#Prototype_new/ZiJun/Hord_Nurakabe/Hord_Nurakabe_Spawner.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hord_Nurakabe_Spawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Nurakabe_Hord = null;
+
+    [SerializeField]
+    Transform m_Max = null;
+
+    [SerializeField]
+    Transform m_Min = null;
+    // Use this for initialization
+
+    public float m_speed_min = 10f;
+    public float m_speed_max = 10f;
+
+    float m_speed = 10f;
+
+    public float m_TimeToSpawn_min = 1f;
+    public float m_TimeToSpawn_max = 1f;
+
+    float m_TimeToSpawn = 0f;
+
+    public bool m_canSpawn;
+
+    float m_countDown = 1f;
+
+    Vector3 m_dir = Vector3.zero;
+
+    bool m_Towards_m_Max = true;
+
+    
+
+    float offsetDistance = 0.5f;
+
+    
+
+    void Start()
+    {
+        //m_Min = transform.GetChild(0);
+        //m_Max = transform.GetChild(1);
+
+        m_countDown = Random.Range(m_TimeToSpawn_min, m_TimeToSpawn_max);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_Towards_m_Max)
+        {
+            m_dir = m_Max.position - this.transform.position;
+            if (m_dir != Vector3.zero)
+            {
+                m_dir.Normalize();
+            }
+            
+        }
+        else
+        {
+            m_dir = m_Min.position - this.transform.position;
+            if (m_dir != Vector3.zero)
+            {
+                m_dir.Normalize();
+            }
+        }
+
+        this.transform.position += m_dir * m_speed * Time.deltaTime;
+
+        if (m_Towards_m_Max)
+        {
+            if ((m_Max.position - this.transform.position).magnitude <= offsetDistance)
+            {
+                m_Towards_m_Max = false;
+            }
+        }
+        else
+        {
+            if ((m_Min.position - this.transform.position).magnitude <= offsetDistance)
+            {
+                m_Towards_m_Max = true;
+            }
+        }
+
+        if (m_canSpawn)
+        {
+            m_countDown -= Time.deltaTime;
+
+            if (m_countDown < 0)
+            {
+                m_TimeToSpawn = Random.Range(m_TimeToSpawn_min, m_TimeToSpawn_max);
+                m_speed = Random.Range(m_speed_min, m_speed_max);
+                m_countDown = m_TimeToSpawn;
+
+                Instantiate<GameObject>(Nurakabe_Hord, this.transform.position, Nurakabe_Hord.transform.rotation);
+
+            }
+        }
+    }
+}

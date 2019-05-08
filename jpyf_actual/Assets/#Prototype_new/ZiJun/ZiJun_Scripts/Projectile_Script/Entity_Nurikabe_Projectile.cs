4be8d9272cb0881507881f5f658ebfb875a82098
ryanhojlz <ryanhojlz@users.noreply.github.com>
2019-05-08 12:36:13 +0800

    Update Entity_Nurikabe_Projectile.cs

--- jpyf_actual/Assets/#Prototype_new/ZiJun/ZiJun_Scripts/Projectile_Script/Entity_Nurikabe_Projectile.cs
+++ jpyf_actual/Assets/#Prototype_new/ZiJun/ZiJun_Scripts/Projectile_Script/Entity_Nurikabe_Projectile.cs
@@ -10,9 +10,11 @@ public class Entity_Nurikabe_Projectile : Entity_Projectile
     public override void HitCart(Collider other)
     {
         //Debug.Log("Nuri Hit");
-        //        GameObject.Find("Stats_ResourceHandler").GetComponent<Stats_ResourceScript>().Cart_TakeDmg((int)(m_dmg));
+        //GameObject.Find("Stats_ResourceHandler").GetComponent<Stats_ResourceScript>().Cart_TakeDmg((int)(m_dmg));
         Stats_ResourceScript.Instance.Cart_TakeDmg((int)m_dmg);
         Push_CartScript.Instance.m_stunDuration = 0.4f;
+
+
     }
 
     public override void HitPlayer(Collider other)

100d46feb0e821c493fcc6cd60e6019d24cb54e1
zijuntok <48231216+zijuntok@users.noreply.github.com>
2019-05-13 13:16:26 +0800

    Push stuff. Added TutorialComplete Achievement

--- jpyf_actual/Assets/#Prototype_new/ZiJun/Achievement & Statistics/All_Achievement/TutorialComplete.cs
+++ jpyf_actual/Assets/#Prototype_new/ZiJun/Achievement & Statistics/All_Achievement/TutorialComplete.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class TutorialComplete : Achievement
 {
+    bool completed = false;
     // Update is called once per frame
     void Update()
     {
-
+        if (completed && !hasDone)//&& TempIndex != index)
+        {
+            hasDone = true;
+            Debug.Log(Achievement_name + " : " + Achievement_descriptions);
+            StartCoroutine(TriggerAchievement());
+        }
     }
 }

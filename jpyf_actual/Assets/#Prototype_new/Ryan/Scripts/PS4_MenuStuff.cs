ecc0c43e235d087f850ce194402fb9403bef1340
ryanhojlz <ryanhojlz@users.noreply.github.com>
2019-05-17 14:30:53 +0800

    iyhuyvhgiuui

--- jpyf_actual/Assets/#Prototype_new/Ryan/Scripts/PS4_MenuStuff.cs
+++ jpyf_actual/Assets/#Prototype_new/Ryan/Scripts/PS4_MenuStuff.cs
@@ -16,6 +16,7 @@ public class PS4_MenuStuff : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+#if UNITY_PS4
         if (PS4_ControllerScript.Instance)
         {
             if (PS4_ControllerScript.Instance.ReturnDpadRight())
@@ -49,4 +50,5 @@ public class PS4_MenuStuff : MonoBehaviour
 
         }
 	}
+#endif
 }
